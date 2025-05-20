2e26c848f67f1bfd77369d35994f9b8a210b5f3a
abdeaitali <abde.aitali@live.com>
2025-05-20 08:58:26 +0200

    update to LCA module

--- LCC/rals_livslangd_python/rail_analysis/LCA.py
+++ LCC/rals_livslangd_python/rail_analysis/LCA.py
@@ -1,26 +1,40 @@
 import pandas as pd
+from rail_analysis.constants import (
+    TECH_LIFE_YEARS,
+    TRACK_LENGTH_M
+)
 
-def get_LCA_renewal (track_length, asset_type, year, share_electricity=1.0):
+
+# Default values
+DEFAULT_SHARE_EL = 0.8  # 80% electricity
+DEFAULT_CIRCULARITY_COEF = 0.0  # 0% circularity
 
 # Emissionfactors kg Co2e per MJ 
-   EF_ELECTRICITY = 0.006464924
-   EF_DIESEL = 0.063583815
+EF_ELECTRICITY = 0.006464924
+EF_DIESEL = 0.063583815
 
-# Read LCA-data och CO2e-valuation
-   lca = pd.read_csv('../data/raw/LCA/lca_base_data.csv', delimiter=';', encoding='utf-8')
-   co2e = pd.read_csv('../data/raw/LCA/co2_valuation.csv', delimiter=';', encoding='utf-8')
+def get_LCA_renewal(asset_type, year, circularity_coef=DEFAULT_CIRCULARITY_COEF, share_electricity=DEFAULT_SHARE_EL):
 
-# Convert commas to dots
-   lca['CO2 emissions_kg/m'] = lca['CO2 emissions_kg/m'].str.replace(',', '.').astype(float)
-   lca['Energy use_MJ_m'] = lca['Energy use_MJ_m'].str.replace(',', '.').astype(float)
-   co2e['CO2_Valuation (SEK/kg Co2e)'] = co2e['CO2_Valuation (SEK/kg Co2e)'].str.replace(',', '.').astype(float)
+   # Read LCA-data och CO2e-valuation
 
-# filter asset
-   row = lca[lca['Asset'] == asset_type].iloc[0]
+   # absolute path
+   my_abs_path = r'C:\Users\AbdouAA\Work Folders\Documents\GitHub\rurut\LCC\rals_livslangd_python\data\raw\LCA\\'
+   lca = pd.read_csv(my_abs_path + 'LCA_indata_EF.csv', delimiter=';', encoding='utf-8')
+   co2e = pd.read_csv(my_abs_path + 'co2_valuation.csv', delimiter=';', encoding='utf-8')
+   # read Energy use emissions file
+   energy_emissions = pd.read_csv(my_abs_path + 'Energy use emissions.csv', delimiter=';', encoding='utf-8')
+
+   # clean up data: replace ',' with '.' and convert to float
+   lca = lca.replace(',', '.', regex=True)
+   co2e = co2e.replace(',', '.', regex=True)
+   energy_emissions = energy_emissions.replace(',', '.', regex=True) 
+
+   # filter asset
+   row = lca[lca['Asset'].str.lower() == asset_type.str.lower()].iloc[0]
    co2_emission = row['CO2 emissions_kg/m']  # kg/m
    energy_use = row['Energy use_MJ_m']       # MJ/m
 
-# get co2e valuation for year 
+   # get co2e valuation for year 
    co2_price = co2e[co2e['Year'] == year]['CO2_Valuation (SEK/kg Co2e)'].values[0]
 
 #convert energy use to co2e - emissions 
@@ -31,9 +45,9 @@ def get_LCA_renewal (track_length, asset_type, year, share_electricity=1.0):
    total_co2_per_m = co2_emission + energy_co2  # kg/m
 
 #total cost 
-   total_cost = total_co2_per_m * co2_price * track_length  # SEK
+   total_cost = total_co2_per_m * co2_price * TRACK_LENGTH_M  # SEK
 
-   return total_cost
+   return total_cost*(1-circularity_coef)  # SEK
 
 # test 
 
